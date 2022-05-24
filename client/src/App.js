781c39e33ca4b7a0033f547536ca8afd58756a78
mrcoder991 <udaygirhepunje41@gmail.com>
2022-05-24 23:18:42 +0530

    pagination done

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { Routes, Route, BrowserRouter, Navigate} from "react-router-dom"
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
     <BrowserRouter>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/auth" element={<Auth />} />
+          <Route exact path="/" element={<Navigate replace={true} to='/posts' />} />
+          <Route exact path="/posts" element={<Home />} />
+          <Route exact path="/posts/search" element={<Home />} />
+          <Route path="/posts:id" element={<PostDetails />}/>
+          <Route exact path="/auth" element={(!user ? <Auth /> : <Navigate to='/posts'/>)} />
         </Routes>
       </Container>
     </BrowserRouter>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import { useDispatch } from 'react-redux';
 import useStyles from '../../styles';
 import { getPosts } from '../../actions/posts';
+import Pagination from '../Pagination';
 
 
 const Home = () => {
@@ -25,6 +26,9 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper elevation={6}>
+                            <Pagination/>
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
