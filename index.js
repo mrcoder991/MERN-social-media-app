1ea33e008216c2df0f82e47e261fcadf2bbb368c
mrcoder991 <udaygirhepunje41@gmail.com>
2022-05-28 15:12:20 +0530

    api end points fixed

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use('/user', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/user', userRoutes);
 
 app.use(express.static(path.join(__dirname, "client", "build")))
 
