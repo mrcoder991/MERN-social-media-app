5ef033cbdb69acb994a0f902c897c5505fd6578e
mrcoder991 <udaygirhepunje41@gmail.com>
2022-06-16 17:30:45 +0530

    navbar dark mode color fix

--- client/src/components/Navbar/Styles.js
+++ client/src/components/Navbar/Styles.js
@@ -10,7 +10,7 @@ export default makeStyles((theme) => ({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: theme.type === 'dark' ? 'rgba(42, 36, 35, 0.85)' : 'rgba(248, 224, 221, 0.85)',
+    backgroundColor: theme.palette.type === 'dark' ? 'rgba(42, 36, 35, 0.85)' : 'rgba(248, 224, 221, 0.85)',
     backdropFilter: 'blur(20px)',
     padding: '5px 20px',
     [theme.breakpoints.down('sm')]: {
